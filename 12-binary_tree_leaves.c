7d740f06a96de3b16ea18f0ba127ca4edce2a293
AlexandreGRN <tulkiidra@gmail.com>
2023-01-09 10:01:11 -0800

    betty coding style fix

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,23 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * searchLeaf - search in the child for existing branch
- * @test: count the number of iteration of the function
+ * searchLeaf - search in the tree for leaf
+ * @tree: tree where to search leaf
  * @i: counter
  */
 void searchLeaf(size_t *i, const binary_tree_t *tree)
 {
+	printf("%ld\n", *i);
+	if (tree->n)
+		printf("%d\n", tree->n);
 	if (tree->left)
 		searchLeaf(i, tree->left);
 	if (tree->right)
 		searchLeaf(i, tree->right);
-	if (!(tree->left && tree->right))
+	if (!(tree->left || tree->right))
 		(*i) = (*i) + 1;
 }
 
 /**
- * binary_tree_leaves - check the tree height
- * @tree: tree we need to check the size of
+ * binary_tree_leaves - check the tree for leaves
+ * @tree: tree
  * Return: size of the tree
  */
 
