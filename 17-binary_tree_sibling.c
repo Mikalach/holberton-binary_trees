3602e34e8eb159b09d45dd01a3b2ce325b76174d
AlexandreGRN <tulkiidra@gmail.com>
2023-01-09 07:44:54 -0800

    Push everything

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,8 +12,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	{
 		if (node == node->parent->left && node->parent->right)
 			return (node->parent->right);
-		else
-			return (NULL);
 		if (node == node->parent->right && node->parent->left)
 			return (node->parent->left);
 		else
